e093174275b59eaa3a9372252c027b5ef0c8ffc9
Sang Cu <sangcn@gmail.com>
2022-08-13 15:37:13 +0700

    feat: add hero

---
+++ 7749/components/WeCanHelp.jsx
@@ -0,0 +1,39 @@
+import BenchmarkIcon from '../assets/benchmark.svg'
+const heros = [
+  {
+    key:1,
+    name: 'LOST OF CONTROL',
+    title: 'Are you delegating your own career development to your company?',
+    icon: BenchmarkIcon
+  },
+  {
+    key:2,
+    name: 'No direction',
+    title: 'You don’t know what next after a few years? your career like its what is it. No plan, no strategy.',
+  },
+  {
+    key:3,
+    name: 'ANXIOUS',
+    title: 'My career go nowhere after a few years. In the next 5 year, I will change my career to something else, just don’t know yet!',
+  },
+]
+
+export default function YourFuture() {
+  return (
+    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {heros.map((hero) => (
+        <li key={hero.key} className="col-span-1 ">
+          <div className="w-full flex items-center justify-between p-6 space-x-6">
+            <div className="flex-1 ">
+              <div className="flex items-center space-x-3">
+        {()=>hero.icon}
+                <h3 className="font-semibold uppercase text-xl">{hero.name}</h3>
+              </div>
+              <p className="mt-1 text-gray-800 ">{hero.title}</p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
--- 7749/pages/index.tsx
+++ 7749/pages/index.tsx
@@ -1,76 +1,52 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Logo from '../assets/logo.svg'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import YourFuture from "../components/YourFuture";
+import WeCanHelp from "../components/WeCanHelp";
+import Logo from "../assets/logo.svg";
+import YourFutureIcon from "../assets/your-future.svg";
+import WeCanHelpIcon from "../assets/we-can-help.svg";
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Createn Next App</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <div>
-        <Logo></Logo>
+      <main className="container mx-auto md max-w-7xl">
+        <div className="relative flex pt-4 items-center justify-between ">
+          <div className="">
+            <Logo />
+          </div>
         </div>
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex justify-center pt-20 pb-40">
+          <div>
+            <h1 className="font-semibold text-4xl text-center ">
+              Helping software engineers thriving in career development
+            </h1>
+          </div>
+        </div>
+        <div className="text-center pb-10">
+          <YourFutureIcon className="mx-auto"></YourFutureIcon>
+          is no where without career development
+        </div>
+        <div className="mx-auto max-w-6xl">
+          <YourFuture />
+        </div>
+        <div className="text-center pt-20">
+          <WeCanHelpIcon className="mx-auto" />
+          The software industry is high demand, it need YOU. Starting to build
+          your career development with our help.
+        </div>
+        <div className="mx-auto max-w-6xl">
+          <WeCanHelp />
         </div>
       </main>
 
@@ -81,12 +57,12 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
